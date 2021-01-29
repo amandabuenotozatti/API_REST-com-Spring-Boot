a80209d3f75d026136695a0ebd58f9ed8b645259
Amanda Tozatti <amandabuenotozatti@gmail.com>
2021-01-28 22:12:03 -0300

    Implementing the DELETE and PUT methods

--- products-apirest/apirest/src/main/java/com/products/apirest/resources/ProductResource.java
+++ products-apirest/apirest/src/main/java/com/products/apirest/resources/ProductResource.java
@@ -3,9 +3,11 @@ package com.products.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +37,15 @@ public class ProductResource {
 	public Product saveProduct(@RequestBody Product product) {
 		return productRepository.save(product);
 	}
-
+	
+	@DeleteMapping("/product")
+	public void deleteProduct(@RequestBody Product product) {
+		productRepository.delete(product);
+	}
+	
+	@PutMapping("/product")
+	public Product updateProduct(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
+	
 }
