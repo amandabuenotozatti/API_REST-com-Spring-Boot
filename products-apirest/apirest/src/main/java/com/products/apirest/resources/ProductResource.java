c05701eb1c694c45ad09a61048e61c2f9fe286ba
Amanda Tozatti <amandabuenotozatti@gmail.com>
2021-01-29 23:16:04 -0300

    Swagger UI

--- products-apirest/apirest/src/main/java/com/products/apirest/resources/ProductResource.java
+++ products-apirest/apirest/src/main/java/com/products/apirest/resources/ProductResource.java
@@ -3,6 +3,7 @@ package com.products.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,35 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
 import com.products.apirest.models.Product;
 import com.products.apirest.repository.ProductRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Products")
+@CrossOrigin(origins="*")
 public class ProductResource {
 	
 	@Autowired
 	ProductRepository productRepository;
 	
 	@GetMapping("/products")
+	@ApiOperation(value="Returns a product's list")
 	public List<Product> productsList(){
 		return productRepository.findAll();
 	}
 	
 	
 	@GetMapping("/product/{id}")
+	@ApiOperation(value="Returns a single product")
 	public Product singleProductList(@PathVariable(value="id") long id){
 		return productRepository.findById(id);
 	}
 	
 	@PostMapping("/product")
+	@ApiOperation(value="Save the product")
 	public Product saveProduct(@RequestBody Product product) {
 		return productRepository.save(product);
 	}
 	
 	@DeleteMapping("/product")
+	@ApiOperation(value="Delete the product")
 	public void deleteProduct(@RequestBody Product product) {
 		productRepository.delete(product);
 	}
 	
 	@PutMapping("/product")
+	@ApiOperation(value="Update the product")
 	public Product updateProduct(@RequestBody Product product) {
 		return productRepository.save(product);
 	}
